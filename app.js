728183cbe56fe49a752b341f7865a944876e1e09
goranterzicjs <goranterzicjs@gmail.com>
2019-04-10 15:48:19 +0200

    ubaceno jos dva teksta neke slike i zapocet deo gde se skrolovanjem menja boja navigacije

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-d
+ var html = document.queruySelector('html');
+ var body = document.queruySelector('body');
+ var nav = document.queruySelector('nav');
+ var navA = document.queruySelector('li a');
+ var naslov = document.getElementByClassName('naslov');
+
+ function scrollFunction() {
+   body.
+ }
+
+ html.addEventListener('scroll',scrollFunction(){
+
+ })
