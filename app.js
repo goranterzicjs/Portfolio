17014a0717c6ebcbd2ee99c50b01b760c00cf267
goranterzicjs <goranterzicjs@gmail.com>
2019-04-19 11:41:10 +0200

    Novija verzija

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //Scroll cod
- var body = document.querySelector('body');
- var nav = document.querySelector('nav');
+var body = document.querySelector('body');
+var nav = document.querySelector('nav');
 
 
- var scrollFunction = function() {
-  var neko =  scrollY / (body.scrollHeight - innerHeight);
+var scrollFunction = function() {
+var neko =  scrollY / (body.scrollHeight - innerHeight);
   return Math.floor(neko * 100);
 };
 
- addEventListener('scroll',function(){
-   nav.style.setProperty('background' , scrollFunction() > 11  ? "var(--color2)" : "var(--color1)");});
+addEventListener('scroll',function(){
+   nav.style.setProperty('background' , scrollFunction() > 1.5  ? "var(--color2)" : "var(--color1)");
+ });
